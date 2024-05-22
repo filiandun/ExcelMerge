11923c2540e067a1792bd8b8d3369fe0ec4f4696
filian <filiandun@gmail.com>
2024-05-22 14:42:16 +0300

    first commit

---
+++ ExcelMerge/ExcelHelper.cs
@@ -0,0 +1,101 @@
+﻿using System.Windows;
+
+using OfficeOpenXml;
+
+namespace ExcelMerge
+{
+    public class ExcelHelper
+    {
+        public static string[] GetSheetNames(ExcelPackage excelPackage)
+        {
+            try
+            {
+                return excelPackage.Workbook.Worksheets.Select(s => s.Name).ToArray();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return Array.Empty<string>();
+            }
+        }
+
+        public static string[] GetColumnNames(ExcelWorksheet excelWorksheet)
+        {
+
+            try
+            {
+                int numberOfColumns = excelWorksheet.Columns.Count();
+
+                string[] columnNames = new string[numberOfColumns];
+                for (int i = 1; i < numberOfColumns; i++)
+                {
+                    columnNames[i - 1] = excelWorksheet.Cells[1, i].Text;   // 1 - номер строки,
+                                                                            // а так как название столбцов необязательно будет на первой строке,
+                                                                            // то нужно сделать проверки на пустоту, пока не найдётся не пустая строка
+                                                                            // или в доп. параметрах добавить, чтобы пользователь мог указать номер строки, где названия столбцов
+                }
+
+                return columnNames;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return Array.Empty<string>();
+            }
+
+        }
+
+        public static async Task<int> CopyingAsync(ExcelWorksheet excelWorksheet1, ExcelWorksheet excelWorksheet2, int column1, int column2, int columnCopy, int columnPaste, CopyOptions copyOptions)
+        {
+            int counter = 0;
+
+            await Task.Run(() =>
+            {
+                for (int i = 1; i <= excelWorksheet1.Dimension.End.Row; i++)
+                {
+                    string cell1Text = excelWorksheet1.Cells[i, column1].Text;
+                    if (string.IsNullOrWhiteSpace(cell1Text)) { continue; }
+
+                    for (int j = 1; j <= excelWorksheet2.Dimension.End.Row; j++)
+                    {
+                        string cell2Text = excelWorksheet2.Cells[j, column2].Text;
+                        if (string.IsNullOrWhiteSpace(cell2Text)) { continue; }
+
+                        string[] preparedCells = ExcelHelper.PrepareCellsForComparison(cell1Text, cell2Text, copyOptions);
+
+                        if (preparedCells[0] == preparedCells[1])
+                        {
+                            excelWorksheet2.Cells[j, columnPaste].Value = excelWorksheet1.Cells[i, columnCopy].Value;
+
+                            if (copyOptions.YellowBackground == true)
+                            {
+                                excelWorksheet2.Cells[j, columnPaste].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+                                excelWorksheet2.Cells[j, columnPaste].Style.Fill.BackgroundColor.SetColor(System.Drawing.Color.Yellow);
+                            }
+
+                            counter++;
+                        }
+                    }
+                }
+            });
+
+            return counter;
+        }
+
+        private static string[] PrepareCellsForComparison(string cell1, string cell2, CopyOptions copyOptions)
+        {
+            if (copyOptions.IgnoreCase == true)
+            {
+                cell1 = cell1.ToLower();
+                cell2 = cell2.ToLower();
+            }
+            if (copyOptions.IgnoreSpace == true)
+            {
+                cell1 = cell1.Trim();
+                cell2 = cell2.Trim();
+            }
+
+            return new string[2] { cell1, cell2 };
+        }
+    }
+}
---
+++ ExcelMerge/MainWindow.xaml.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+using System.Windows.Controls;
+using System.IO;
+
+using OfficeOpenXml;
+
+
+namespace ExcelMerge
+{
+    public partial class MainWindow : Window
+    {
+        private ExcelPackage excelPackage1;
+        private ExcelPackage excelPackage2;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial; // иначе Exception
+        }
+
+
+        private void BtnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog()
+            {
+                Filter = "Excel Files (*.xlsx, *.xlsm)|*.xlsx;*.xlsm",
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                FileInfo fileInfo = new FileInfo(openFileDialog.FileName);
+
+                Button button = sender as Button;
+                if (button == null) { return; };
+
+                switch (button.Name)
+                {
+                    case "btnOpenFile1":
+                        this.OpenExcelFile(ref this.excelPackage1, fileInfo, this.txtFile1, this.cmbSheet1);
+                        break;
+
+                    case "btnOpenFile2":
+                        this.OpenExcelFile(ref this.excelPackage2, fileInfo, this.txtFile2, this.cmbSheet2);
+                        break;
+                }
+            }
+        }
+
+        private void OpenExcelFile(ref ExcelPackage excelPackage, FileInfo fileInfo, TextBlock textBox, ComboBox comboBox)
+        {
+            excelPackage = new ExcelPackage(fileInfo);
+
+            textBox.Text = $"{fileInfo.Name} ({fileInfo.FullName})";
+            comboBox.ItemsSource = ExcelHelper.GetSheetNames(excelPackage);
+        }
+
+
+
+
+        private void CmbSheet_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = sender as ComboBox;
+            if (comboBox == null || comboBox.SelectedIndex == -1) return;
+
+            switch (comboBox.Name)
+            {
+                case "cmbSheet1":
+                    LoadSheetColumns(this.excelPackage1, comboBox, this.cmbColumn1, this.cmbCopyColumn1);
+                    break;
+
+                case "cmbSheet2":
+                    LoadSheetColumns(this.excelPackage2, comboBox, this.cmbColumn2, this.cmbPasteColumn2);
+                    break;
+            }
+        }
+
+        private void LoadSheetColumns(ExcelPackage excelPackage, ComboBox sheetComboBox, ComboBox columnComboBox1, ComboBox columnComboBox2)
+        {
+            if (excelPackage == null) return;
+
+            ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets[sheetComboBox.SelectedItem.ToString()];
+
+            columnComboBox1.ItemsSource = ExcelHelper.GetColumnNames(worksheet);
+            columnComboBox2.ItemsSource = ExcelHelper.GetColumnNames(worksheet);
+        }
+
+
+
+
+        private async void btnCopy_Click(object sender, RoutedEventArgs e)
+        {
+            if (!ValidateInputs())
+            {
+                MessageBox.Show("Пожалуйста, выберите все необходимые параметры.");
+            }
+
+            var sheet1 = this.excelPackage1.Workbook.Worksheets[this.cmbSheet1.SelectedItem.ToString()];
+            var sheet2 = this.excelPackage2.Workbook.Worksheets[this.cmbSheet2.SelectedItem.ToString()];
+
+            int column1 = this.cmbColumn1.SelectedIndex + 1;
+            int column2 = this.cmbColumn2.SelectedIndex + 1;
+
+            int columnCopy = this.cmbCopyColumn1.SelectedIndex + 1;
+            int columnPaste = this.cmbPasteColumn2.SelectedIndex + 1;
+
+            CopyOptions copyOptions = new CopyOptions(this.cbIngoreCase.IsChecked, this.cbIngoreSpace.IsChecked, this.cbYellowBackground.IsChecked);
+
+            int counter = await ExcelHelper.CopyingAsync(sheet1, sheet2, column1, column2, columnCopy, columnPaste, copyOptions);
+            this.excelPackage2.Save();
+
+            MessageBox.Show($"Копирование завершено {counter}"); // counter не совсем корретно работает, так как содержимое ячеек может повторяться
+        }
+
+        private bool ValidateInputs()
+        {
+            return this.excelPackage1 != null && this.excelPackage2 != null &&
+                   this.cmbSheet1.SelectedItem != null && this.cmbSheet2.SelectedItem != null &&
+                   this.cmbColumn1.SelectedIndex != -1 && this.cmbColumn2.SelectedIndex != -1 &&
+                   this.cmbCopyColumn1.SelectedIndex != -1 && this.cmbPasteColumn2.SelectedIndex != -1;
+        }
+    }
+
+
+    public struct CopyOptions
+    {
+        public bool? IgnoreCase { get; set; }
+        public bool? IgnoreSpace { get; set; }
+        public bool? YellowBackground { get; set; }
+
+        public CopyOptions(bool? ignoreCase, bool? ignoreSpace, bool? yellowbackground) 
+        {
+            this.IgnoreCase = ignoreCase;
+            this.IgnoreSpace = ignoreSpace;
+            this.YellowBackground = yellowbackground;
+        }
+    }
+}
